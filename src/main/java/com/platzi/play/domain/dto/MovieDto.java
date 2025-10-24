7bb6756ec8452046d8c41034c504826845738310
Armando Ochoa <o.armando98@gmail.com>
2025-10-23 18:21:39 -0600

    crear endpoint post para guardar movie en spring boot

--- src/main/java/com/platzi/play/domain/dto/MovieDto.java
+++ src/main/java/com/platzi/play/domain/dto/MovieDto.java
@@ -3,6 +3,7 @@ package com.platzi.play.domain.dto;
 import java.time.LocalDate;
 
 public record MovieDto(
+        Long id,
         String title,
         Integer duration,
         String genre,
--- src/main/java/com/platzi/play/persistence/mapper/MovieMapper.java
+++ src/main/java/com/platzi/play/persistence/mapper/MovieMapper.java
@@ -2,6 +2,7 @@ package com.platzi.play.persistence.mapper;
 
 import com.platzi.play.domain.dto.MovieDto;
 import com.platzi.play.persistence.entity.MovieEntity;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -17,4 +18,9 @@ public interface MovieMapper {
     @Mapping(source = "estado", target = "status", qualifiedByName = "stringToBoolean")
     MovieDto toDto(MovieEntity entity);
     List<MovieDto> toDto(Iterable<MovieEntity> entities);
+
+    @InheritInverseConfiguration
+    @Mapping(source = "genre", target = "genero", qualifiedByName = "genreToString")
+    @Mapping(source = "status", target = "estado", qualifiedByName = "booleanToString")
+    MovieEntity toEntity(MovieDto dto);
 }
